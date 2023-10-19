88c84b40999762eef7961e0b99e418e4dbe06b5b
alotav <alonsotaverna@hotmail.com>
2023-10-19 19:54:24 -0300

    vistas get delete update

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from apps.products.api.views.general_views import MeasureUnitListAPIView, IndicatosListAPIView, CategoryProductListAPIView
-from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView
+from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView, ProductUpdateAPIView
 
 
 urlpatterns = [
@@ -10,4 +10,7 @@ urlpatterns = [
     path('category_product/', CategoryProductListAPIView.as_view(), name = 'category_product'),
     path('product/list/', ProductListAPIView.as_view(), name = 'product_list'),
     path('product/create/', ProductCreateAPIView.as_view(), name = 'product_create'),
+    path('product/retrieve/<int:pk>', ProductRetrieveAPIView.as_view(), name = 'product_retrieve'),
+    path('product/destroy/<int:pk>', ProductDestroyAPIView.as_view(), name = 'product_destroy'),
+    path('product/update/<int:pk>', ProductUpdateAPIView.as_view(), name = 'product_update'),
 ]
